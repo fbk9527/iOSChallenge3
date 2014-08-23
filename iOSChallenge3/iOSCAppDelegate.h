773f1a53dff20f315e60b7c1e2f2230a37c9d45b
fbk9527 <fbk9527@rit.edu>
2014-08-23 01:58:51 -0400

    Initial Commit

---
+++ iOSChallenge3/iOSCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iOSCAppDelegate.h
+//  iOSChallenge3
+//
+//  Created by Freddy kelch on 8/23/14.
+//  Copyright (c) 2014 freddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSChallenge3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSChallenge3
+//
+//  Created by Freddy kelch on 8/23/14.
+//  Copyright (c) 2014 freddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iOSCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iOSCAppDelegate class]));
+    }
+}
